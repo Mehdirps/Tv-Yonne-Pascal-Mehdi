0d84ac70528be6467e02fa3a354fffcef941985d
Mehdirps <Mehdi.raposo77@gmail.com>
2021-10-19 16:55:59 +0200

    debut page dashbord

---
+++ includes/dashbord_nav.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="lien">
+        <ul>
+            <li>City</li>
+            <li>Emissions</li>
+            <li>Formulaires</li>
+            <li>Newsletter</li>
+            <li>Partners</li>
+            <li>Socials</li>
+            <li>Staffs</li>
+            <li>Users</li>
+            <li>Videos</li>
+        </ul>
+    </div>
+</nav>
